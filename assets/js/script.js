8ba5828461e3b3c22a8b05d7c4ad72eb2f5eecb1
ifoy03 <marcu.sorin.2003@gmail.com>
2019-11-16 06:16:58 +0300

    global 2

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+/* Поп ап */
+function popup(){
+	var all = document.getElementById('all');
+	var pop_up = document.getElementById('pop_up');
+	all.style.overflow ="hidden";
+	pop_up.style.visibility = "visible"
+}
+window.onclick = function(e){
+	if (e.target == pop_up){
+		pop_up.style.visibility = "hidden";
+		all.style.overflow ="visible";
+	}
+}
+
+/* Якори покрутки */
+$('a[href^="#"]').bind("click", function(e){
+  var anchor = $(this);
+  $('html, body').stop().animate({
+   scrollTop: $(anchor.attr("href")).offset().top
+  }, 1000);
+  e.preventDefault();
+ });
+ /* Текст */
+ function textMore(){
+ 	var txt = document.getElementById('txtMore');
+ 	var dots = document.getElementById('dotsMore');
+ 	var hidden = document.getElementById('hidden_moreText');
+ 	hidden_moreText.style.display = "block";
+ 	txt.style.display = "block";
+ 	dots.style.display = "none";
+ }
+ function hiden(){
+ 	var txt = document.getElementById('txtMore');
+ 	var dots = document.getElementById('dotsMore');
+ 	var hidden = document.getElementById('hidden_moreText');
+ 	hidden_moreText.style.display = "none";
+ 	txt.style.display = "none";
+ 	dots.style.display = "block";
+
+ }
